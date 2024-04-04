8b2c1230903e5ec7644af66bdd826fbe4906e393
K.K.RAKITHA AMADITHYA <137595471+RAKITHA-AMADITHYA@users.noreply.github.com>
2024-04-05 02:14:48 +0530

    updated the layout

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -2,24 +2,13 @@ import { Box } from '@mui/material';
 import Header from './Header';
 import { Outlet } from 'react-router-dom';
 
-// function Layout() {
-//   return (
-//     <div style={{ display: 'flex', flexDirection: 'column'}}>
-//       <Header />
-//       <div style={{ flex: 50 }}>
-//         <Outlet />
-//       </div>
-//     </div>
-//   );
-// }
 function Layout() {
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
-      <Header />
-      {/* <Box sx={{ display: 'flex', flexGrow: 1, overflow: 'hidden',backgroundColor:'#eeeeee' }}> */}
-        <Outlet/>
-        
-      {/* </Box> */}
+      <Header sx={{ flex: '0 0 auto', height: '64px' }} />
+      <Box sx={{ flex: '1 1 auto', overflowY: 'auto' }}>
+        <Outlet />
+      </Box>
     </Box>
   );
 }
