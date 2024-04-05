e1caf0a5c918d03ff52bbc78ff950f57211408c0
K.K.RAKITHA AMADITHYA <137595471+RAKITHA-AMADITHYA@users.noreply.github.com>
2024-04-05 10:31:52 +0530

    updated container form

--- src/components/ContainerForm/ContainerForm.jsx
+++ src/components/ContainerForm/ContainerForm.jsx
@@ -3,26 +3,26 @@ import React from 'react'
 
 const ContainerForm = (props) => {
   return (
-    <Grid container p={2}>
+    <Grid container justifyContent="center" alignItems="center" >
         <Box
-                sx={{
-                    backgroundColor: '#fafafa',
-                    borderRadius: 2,
-                    boxShadow: '0 2px 4px rgba(0,0,0,0.1)',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    width: '100%',
-                    height: '100%',
-                    p: 2,
-                    
-
-                }}
-            >
-
-                {props.children}
-            </Box>
-    </Grid>
-
+    sx={{
+      backgroundColor: '#f5f5f5',
+      borderRadius: 2,
+      boxShadow: '0 2px 4px rgba(0,0,0,0.1)',
+      display: 'flex',
+      flexDirection: 'column',
+      width: '100%', // Full width on xs and sm screens
+      height: '100%',
+      p: 2,
+      [`@media (min-width: 1280px)`]: {
+        // 1280px is lg breakpoint
+        width: '60%', // 60% width on lg and xl screens
+      },
+    }}
+  >
+        {props.children}
+      </Box>
+   </Grid>
   )
 }
 
