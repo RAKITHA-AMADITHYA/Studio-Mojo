774d2aa6ed08a03222103058ebfc286a1fd7d09f
K.K.RAKITHA AMADITHYA <137595471+RAKITHA-AMADITHYA@users.noreply.github.com>
2024-04-05 10:32:40 +0530

    updated business account jsx

--- src/pages/BusinessAccount.jsx
+++ src/pages/BusinessAccount.jsx
@@ -20,9 +20,9 @@ const BusinessAccount = () => {
     });
 
     return (
-        <div style={{ padding: 10 }}>
+        <div style={{ padding: 10, display: 'flex', justifyContent: 'center', alignItems: 'center' }} >
             <ContainerForm>
-                <Typography sx={{ mt: 3, color: "#616161" }} variant='h6'>Create Business Account</Typography>
+                <Typography sx={{ mt: 3, color: "#616161" }} variant='h3'>Create Business Account</Typography>
                 <Divider sx={{ my: 2 }} />
 
                 <Grid container spacing={2} rowGap={2}>
@@ -36,6 +36,7 @@ const BusinessAccount = () => {
                         <TextField id="outlined-basic" label="Last Name" variant="outlined" fullWidth />
                     </Grid>
 
+
                     {/* Email field */}
                     <Grid item xs={12} sm={12} md={6} lg={6} xl={4} flexDirection={'column'}>
                         <TextField id="outlined-basic" label="Email" variant="outlined" fullWidth />
@@ -46,10 +47,11 @@ const BusinessAccount = () => {
                         <TextField id="outlined-basic" label="Contact" variant="outlined" fullWidth />
                     </Grid>
 
-                    {/* BR Code field */}
+                    {/* BRD Code field */}
                     <Grid item xs={12} sm={12} md={6} lg={6} xl={4} flexDirection={'column'}>
-                        <TextField id="outlined-basic" label="BR Code" variant="outlined" fullWidth />
+                        <TextField id="outlined-basic" label="BRD Code" variant="outlined" fullWidth />
                     </Grid>
+
 
                     {/* Password field */}
                     <Grid item xs={12} sm={12} md={6} lg={6} xl={4} flexDirection={'column'}>
@@ -61,28 +63,31 @@ const BusinessAccount = () => {
                         <TextField id="outlined-basic" label="Confirm Password" variant="outlined" fullWidth />
                     </Grid>
 
-                    {/* Upload File button */}
-                    <Grid item xs={12} sm={12} md={6} lg={6} xl={4} flexDirection={'column'} mt={2}>
+                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12} flexDirection={'column'} >
+
+                        <Typography sx={{ mt: 3, color: "#616161" }} variant='h5'>Attachments</Typography>
+                        <Typography sx={{ mt: 1, color: "#616161" }} variant='p'>Upload if you have any documents</Typography>
+
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12} flexDirection={'column'} display={'flex'} justifyContent={'center'}>
                         <Button
                             component="label"
                             role={undefined}
-                            variant="contained"
+                            variant="outlined"
                             tabIndex={-1}
                             startIcon={<CloudUploadIcon />}
-                            fullWidth
-                            sx={{
-                                '&:hover': {
-                                    backgroundColor: '#1976d2', // Primary color
-                                },
-                            }}
+                            
                         >
-                            Upload File
+                            Upload file
                             <VisuallyHiddenInput type="file" />
                         </Button>
                     </Grid>
 
+
+
+
                     {/* Terms & Conditions Checkbox */}
-                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12} flexDirection={'column'} mt={2}>
+                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12} flexDirection={'column'} >
                         <FormControlLabel
                             control={<Checkbox />}
                             label="I have read and agree to Terms & Conditions"
@@ -90,24 +95,25 @@ const BusinessAccount = () => {
                     </Grid>
 
                     {/* Create Account Button */}
-                    <Grid item xs={12} sm={12} md={12} lg={12} xl={4} flexDirection={'column'} mt={2} display={'flex'} justifyContent={'center'}>
-                        <Button
-                            variant="contained"
-                            disableElevation
-                            sx={{
-                                '&:hover': {
-                                    backgroundColor: '#1976d2', // Primary color
-                                },
-                            }}
-                        >
-                            Create Account
-                        </Button>
-                    </Grid>
-
                     <Grid item xs={12} sm={12} md={12} lg={12} xl={4} flexDirection={'column'} mt={2} display={'flex'} justifyContent={'center'} alignItems={'center'}>
+  <Button
+    variant="contained"
+    disableElevation
+    sx={{
+      '&:hover': {
+        backgroundColor: '#1976d2', // Primary color
+      },
+      width: '50%', // Set width to fit the content
+    }}
+  >
+    Create Account
+  </Button>
+</Grid>
+
+                    <Grid item xs={12} sm={12} md={12} lg={12} xl={4} flexDirection={'column'}  display={'flex'} justifyContent={'center'} alignItems={'center'}>
 
                         {/* Register Link */}
-                        <Typography variant="subtitle1" color="primary" sx={{ mt: 2 }}>
+                        <Typography variant="subtitle1" color="primary" >
                             <Link to="/login" style={{ textDecoration: 'none', color: 'inherit' }}>Already have an account ? Log In</Link>
                         </Typography>
                     </Grid>
