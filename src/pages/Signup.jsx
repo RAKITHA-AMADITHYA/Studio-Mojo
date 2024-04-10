7de88cb6b0a27a4edf4d35146637e26dd01f3d4d
K.K.RAKITHA AMADITHYA <137595471+RAKITHA-AMADITHYA@users.noreply.github.com>
2024-04-10 14:49:48 +0530

    updated signup page

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -17,8 +17,8 @@ const Signup = () => {
                 p: 1
             }}>
                 <Grid item display={'flex'} justifyContent={'center'} flexDirection={'column'} textAlign={'center'} p={5}>
-                    <Typography variant='h2'>Create Account</Typography>
-                    <Typography variant='subtitle1'>
+                    <Typography variant='h2' sx={{ fontSize: { xs: '30px', sm: '40px', md: '40px', lg: '50px', xl: '80px' }, }}>Create Account</Typography>
+                    <Typography variant='subtitle1' sx={{ fontSize: { xs: '12px', sm: '15px', md: '15px', lg: '15px', xl: '30px' } }}>
                         <span style={{ fontSize: 20 }}>
                             Unlock a World of Possibilities.
                         </span>{" "}
@@ -30,7 +30,7 @@ const Signup = () => {
 
                     <Grid container spacing={2} mt={2}>
                         <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
-                            <Link to="/signup/personal-account">
+                            <Link to="/signup/personal-account" style={{ textDecoration: 'none' }}>
                                 <Box
                                     sx={{
                                         borderRadius: 2,
@@ -42,8 +42,7 @@ const Signup = () => {
                                         alignItems: 'center',
                                         p: 2,
                                         '&:hover': {
-                                            backgroundColor: '#0070C0',
-                                            color: 'white',
+                                            backgroundColor: '#CEE5F3',
                                             cursor: 'pointer',
                                         },
                                     }}
@@ -54,7 +53,7 @@ const Signup = () => {
                             </Link>
                         </Grid>
                         <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
-                            <Link to="/signup/business-account">
+                            <Link to="/signup/business-account" style={{ textDecoration: 'none' }}>
                                 <Box
                                     sx={{
                                         borderRadius: 2,
@@ -66,8 +65,7 @@ const Signup = () => {
                                         alignItems: 'center',
                                         p: 2,
                                         '&:hover': {
-                                            backgroundColor: '#0070C0',
-                                            color: 'white',
+                                            backgroundColor: '#CEE5F3',
                                             cursor: 'pointer',
                                         },
                                     }}
@@ -77,6 +75,7 @@ const Signup = () => {
                                 </Box>
                             </Link>
                         </Grid>
+
                     </Grid>
 
                 </Grid>
