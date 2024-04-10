b3d9a124db6406552de4be3b18a7040edca40885
K.K.RAKITHA AMADITHYA <137595471+RAKITHA-AMADITHYA@users.noreply.github.com>
2024-04-10 10:32:28 +0530

    created the card component

---
+++ src/components/ContentCard/Card.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Grid, Typography} from '@mui/material'
+
+const Card = (props) => {
+  return (
+           <Grid
+            item
+            xs={12}
+            sm={6}
+            md={3}
+            lg={3}
+            xl={3}
+            container
+            justifyContent="center"
+            alignItems="center"
+            flexDirection="column"
+          >
+            <img src={props.img} width={'auto'} alt="" />
+            <Typography variant="h5" sx={{ fontSize: { xs: '25px', sm: '25px', md: '20px', lg: '22px', xl: '28px' },mt:2 }} fontWeight={600}>{props.title}<br/>{props.title2}</Typography>
+            <Grid item p={2}>
+              <Typography variant="subtitle1" sx={{ fontSize: { xs: '18px', sm: '18px', md: '18px', lg: '18px', xl: '25px' } }}>
+                {props.description}
+                
+               </Typography>
+              
+
+                </Grid>
+          </Grid>
+         
+
+  )
+}
+
+export default Card
