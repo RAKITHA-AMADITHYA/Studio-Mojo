18e8a42975e237159662b1ce0e3292e7a9551730
K.K.RAKITHA AMADITHYA <137595471+RAKITHA-AMADITHYA@users.noreply.github.com>
2024-04-10 15:57:12 +0530

    updated home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,6 +14,7 @@ import { useGSAP } from '@gsap/react'
 import gsap from "gsap";
 import { Link } from 'react-router-dom'
 import Card from '../components/ContentCard/Card'
+import Footer from '../components/footer/Footer'
 
 const Home = () => {
 
@@ -72,15 +73,15 @@ const Home = () => {
 
             <Link to="/signup">
               <Fade in timeout={4000}>
-              <Button
-  sx={{
-    mt: 4, // Add margin-top
-    fontSize: { xs: '1rem', xl: '1.5rem' }, // Set different font sizes for different screen sizes
-    padding: { xs: '10px 20px', xl: '20px 40px' }, // Set different padding for different screen sizes
-  }}
-  variant="outlined"
-  href="#outlined-buttons"
->
+                <Button
+                  sx={{
+                    mt: 4, // Add margin-top
+                    fontSize: { xs: '1rem', xl: '1.5rem' }, // Set different font sizes for different screen sizes
+                    padding: { xs: '10px 20px', xl: '20px 40px' }, // Set different padding for different screen sizes
+                  }}
+                  variant="outlined"
+                  href="#outlined-buttons"
+                >
                   Get Started
                 </Button>
               </Fade>
@@ -97,34 +98,34 @@ const Home = () => {
         </Grid>
         <Grid container p={2} spacing={2} display={'flex'} justifyContent={'center'} alignItems={'center'} textAlign={'center'}>
 
-        <Card img={messegeIcon} title="SMS Campaigns to " title2="Contact Lists"
-          description="You can send a simple SMS message to multiple contact groups at the same
+          <Card img={messegeIcon} title="SMS Campaigns to " title2="Contact Lists"
+            description="You can send a simple SMS message to multiple contact groups at the same
         time in one single campaign."
-        />
+          />
 
-        <Card img={missedCallIcon}   title="Missed Call lead  " title2="generation"
-          description="You can send a simple SMS message to multiple contact groups at the same
+          <Card img={missedCallIcon} title="Missed Call lead  " title2="generation"
+            description="You can send a simple SMS message to multiple contact groups at the same
         time in one single campaign."
-        />
+          />
 
-<Card img={robotIcon} title="Bots with automated  "  title2=" interactivity"
-          description="You can send a simple SMS message to multiple contact groups at the same
+          <Card img={robotIcon} title="Bots with automated  " title2=" interactivity"
+            description="You can send a simple SMS message to multiple contact groups at the same
         time in one single campaign."
-        />
-        
-        <Card img={noteIcon} title="Criteria system for "  title2=" campaigns"
-          description="You can send a simple SMS message to multiple contact groups at the same
-        time in one single campaign."
-        />
+          />
 
-</Grid>
+          <Card img={noteIcon} title="Criteria system for " title2=" campaigns"
+            description="You can send a simple SMS message to multiple contact groups at the same
+        time in one single campaign."
+          />
+
+        </Grid>
 
         <Grid item p={2} display={'flex'} justifyContent={'center'} >
           <Link to="/features">
             <Button
               sx={{
                 mt: 2, // Add margin-top
-                fontSize: { xs: '1rem', xl: '1.5rem' },                padding: '10px 20px',
+                fontSize: { xs: '1rem', xl: '1.5rem' }, padding: '10px 20px',
                 '&:hover': {
                   backgroundColor: '#0070C0', // Change background color on hover
                 }, // Increase padding
@@ -165,69 +166,45 @@ const Home = () => {
         </Grid>
 
 
-        
+
       </section>
 
       {/* Section 4 */}
       <section style={{ background: 'var(--P1, rgba(0, 112, 192, 0.06))' }}>
-        <Grid item display={'flex'} justifyContent={'center'} alignItems={'center'} textAlign={'center'} p={2}>
-          <Typography variant='h4'  sx={{ fontSize: { xs: '25px', sm: '25px', md: '30px', lg: '30px', xl: '30px' } }} fontWeight={400}>Convenient and cost-effective plans are available for Anyone</Typography>
+        <Grid item display={'flex'} justifyContent={'center'} alignItems={'center'} textAlign={'center'} p={2} mt={5}>
+          <Typography variant='h4' sx={{ fontSize: { xs: '18px', sm: '18px', md: '18px', lg: '18px', xl: '20px' } }} fontWeight={400}>Convenient and cost-effective plans are available for Anyone</Typography>
         </Grid>
 
 
-        <Grid item display={'flex'} justifyContent={'center'} alignItems={'center'} textAlign={'center'} mt={2} p={2}>
-          <Typography variant='h3' sx={{ fontSize: { xs: '30px', sm: '35px', md: '35px', lg: '35px', xl: '35px' } }}>Have An Unlimited Amount Of Team Members, Contacts And Text Messages</Typography>
+        <Grid item display={'flex'} justifyContent={'center'} alignItems={'center'} textAlign={'center'}  p={2}>
+          <Typography variant='h3'fontWeight={800} sx={{ fontSize: { xs: '30px', sm: '35px', md: '35px', lg: '35px', xl: '35px' } }}>Have An Unlimited Amount Of Team Members, Contacts<br/> And Text Messages</Typography>
         </Grid>
 
-        <Grid item display={'flex'} justifyContent={'center'} alignItems={'center'} mt={20} p={2}>
+        <Grid item display={'flex'} justifyContent={'center'} alignItems={'center'} mt={10} p={2}>
           <Typography variant='h3' color={'#0070C0'}>No Setup Fees</Typography>
+        </Grid>
+
+        <Grid item p={2} display={'flex'} justifyContent={'center'} >
+          <Link to="/pricing">
+            <Button
+              sx={{
+                fontSize: { xs: '1rem', xl: '1.5rem' }, padding: '10px 20px',
+                '&:hover': {
+                  backgroundColor: '#0070C0', // Change background color on hover
+                }, // Increase padding
+              }}
+              variant="contained"
+              href="#outlined-buttons"
+
+            >
+              See Pricing
+            </Button>
+          </Link>
         </Grid>
       </section>
 
       {/* footer */}
-      <section style={{ background: '#0070C0' }}>
-        <Grid container p={2}>
-          <Grid item xs={6} sm={6} md={6} lg={6} xl={6} >
-            <img src={logo} width={'30%'} alt="" />
-          </Grid>
-
-          <Grid item xs={6} sm={6} md={6} lg={6} xl={6} display={'flex'} justifyContent={'end'} alignItems={'end'} flexDirection={'column'}>
-            <Typography variant='p' color={'white'} fontWeight={400}>Contact</Typography>
-            <Typography variant='p' color={'white'} fontWeight={600}>(+94) 70 000 0000</Typography>
-
-
-            <Typography variant='subtitle1' color={'white'} fontWeight={400} >Email</Typography>
-            <Typography variant='subtitle1' color={'white'} fontWeight={600}>support@studiomojo.lk</Typography>
-          </Grid>
-
-        </Grid>
-        <Divider sx={{ my: 2 }} />
-        <Grid item display={'flex'} justifyContent={'start'} alignItems={'start'} spacing={2} p={1}>
-          <Typography variant='p' color={'white'} fontWeight={400}>© 2023 StudioMojo</Typography>
-
-        </Grid>
-
-        <Grid item display={'flex'} justifyContent={'center'} alignItems={'center'} spacing={2}>
-          <Typography variant='p' color={'white'} fontWeight={500}>Terms & Conditions</Typography>
-          <Box mx={2} />
-          <Typography variant='p' color={'white'} fontWeight={500}>Privacy Policy</Typography>
-        </Grid>
-
-        <Grid container justify="space-between" alignItems="end" justifyContent={'end'} spacing={5}>
-          <Grid item>
-            <FacebookIcon style={{ color: 'white' }} />
-          </Grid>
-          <Grid item>
-            <YouTubeIcon style={{ color: 'white' }} />
-          </Grid>
-          <Grid item>
-            <InstagramIcon style={{ color: 'white' }} />
-          </Grid>
-        </Grid>
-
-
-
-      </section>
+      <Footer/>
 
     </div>
   )
