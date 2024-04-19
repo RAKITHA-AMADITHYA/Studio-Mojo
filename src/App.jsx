8e58a47baab9db2bd2be6e279f5d8a53bd82fb1e
K.K.RAKITHA AMADITHYA <137595471+RAKITHA-AMADITHYA@users.noreply.github.com>
2024-04-19 13:55:22 +0530

    updated the lorder

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { BarLoader } from "react-spinners";
 import ThemeProvider from "./theme";
 import CircularProgress from '@mui/material/CircularProgress';
-
+import Loader from 'react-loaders';
+import 'loaders.css/src/animations/line-scale.scss';
 
 const Home = lazy(() => import("./pages/Home"));
 const Layout = lazy(() => import("./layouts/Layout"));
@@ -17,35 +18,19 @@ const PersonalAccount = lazy(() => import("./pages/PersonalAccount"));
 const BusinessAccount = lazy(() => import("./pages/BusinessAccount"));
 const Error1 = lazy(() => import("./components/error/Error1"));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 function App() {
   return (
     <ThemeProvider>
-
       <Router>
-      <Suspense
-  fallback={
-    <div style={{ width: "100%", display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
-      <CircularProgress color="primary" />
-    </div>
-  }
->
+        <Suspense
+          fallback={
+            <div style={{ width: "100%", display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+              <Loader type="line-scale" color="#0070C0" active />
+            </div>
+          }
+        >
           <Routes>
-              <Route path="/*" element={<Error1 />} />
+            <Route path="/*" element={<Error1 />} />
             <Route element={<Layout />}>
               <Route path="/" element={<Home />} />
               <Route path="features" element={<Features />} />
@@ -56,14 +41,11 @@ function App() {
               <Route path="signup" element={<Signup />} />
               <Route path="signup/personal-account" element={<PersonalAccount />} />
               <Route path="signup/business-account" element={<BusinessAccount />} />
-
-
-
-
             </Route>
           </Routes>
         </Suspense>
-      </Router></ThemeProvider>
+      </Router>
+    </ThemeProvider>
   );
 }
 
